465ad5ef2200b6cb43cb57f666c97cbfa942c761
Slackware Live User <live@darkstar.example.net>
2017-04-06 23:50:23 -0700

    C

--- Functions.h
+++ Functions.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+\ integrales \
+
 double simpson(double (*f)(double), double b, double a, double n)
 {
   double sum1=0, sum2=0, sum3=0, c;
@@ -20,7 +22,9 @@ double simpson(double (*f)(double), double b, double a, double n)
 
   return sum3;
 }
-    
+
+\otras\
+
 double fibonacci(double x)
 {
   if(n==1||n==2)
@@ -39,6 +43,8 @@ long factorial(int x)
     return n*factorial(n-1);
 }
 
+\derivadas\
+
 double forward(double (*f)(double), double x, double h)
 {
   return (f(x+h)-f(x))/h;
@@ -50,7 +56,7 @@ double backward(double (*f)(double), double x, double h)
   return (f(x)-f(x-h))/h;
 }
 
-double halfp(double (*f)(double), double x, double h)
+double central(double (*f)(double), double x, double h)
 
 {
   return (f(x+h/2)-f(x-h/2))/h;
@@ -58,8 +64,56 @@ double halfp(double (*f)(double), double x, double h)
 
 double richardson(double x, double h)
 {
-  return (4*halfp(x, h/2)-halfp(x, h))/3;
+  return (4*central(x, h/2)-central(x, h))/3;
 }
 
+\raices\
 
+double fixed_point(double (*f)(double), double (*g)(double), double x, int MAX, double eps)
+{
+  double c, d;
+  for (int ii=0; ii<=MAX; ii++)
+    {
+      c=g(x);
+      if (std::fabs(f(c))<eps)
+        {
+          break;
+        }
+      d=1-c/x;
+      std::cout<<ii<<"\t"<<c<<"\t"<<f(c)<<"\t"<<d<<std::endl;
+      x=c;
+    }
+  return c;
+}
 
+double secante(double (*f)(double), double po, double pi, int MAX, double eps)
+{
+  double p;
+  for (int a=0; a<=MAX; a++)
+    {
+      p=pi-(f(pi)*(po-pi)/(f(po)-f(pi)));
+      if (std::fabs(f(p))<eps)
+	{
+	  break;
+	}
+      po=pi;
+      pi=p;
+    }
+  return p;
+}
+
+double newton(double (*f)(double), double (*fp)(double), double po,int MAX,double eps)
+{
+  double p;
+  for(int a=0;a<=MAX; a++)
+    {
+      p=po-(f(po))/(fp(po));
+      
+      if (std::fabs(f(p))<eps)
+        {
+          break;
+        }
+      po=p;
+    }
+  return p;
+}
